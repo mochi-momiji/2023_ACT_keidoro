962e487cf54c9e20f6b7429102d9aba62ef26269
itou aiki <itouaiki46@gmail.com>
2023-09-15 14:22:26 +0900

    ステージのプレハブをメインのフォルダに追加

--- 2023_ACT_keidoro/Assets/Debug/Scripts/itou/ItemSpeedUp.cs
+++ 2023_ACT_keidoro/Assets/Debug/Scripts/itou/ItemSpeedUp.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ItemSpeedUp : MonoBehaviour
 {
+    [SerializeField] private GameObject playerObject = null;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,13 @@ public class ItemSpeedUp : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            playerObject.GetComponent<PlayerController>().isBoost = true;
+            Destroy(this.gameObject);
+        }
     }
 }
