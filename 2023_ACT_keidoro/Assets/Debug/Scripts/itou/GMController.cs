712b165b210fd5ee82153e33ef7b8edb538cfaef
itou aiki <itouaiki46@gmail.com>
2023-09-15 15:03:44 +0900

    タイマーとシーンの設定

--- 2023_ACT_keidoro/Assets/Debug/Scripts/itou/GMController.cs
+++ 2023_ACT_keidoro/Assets/Debug/Scripts/itou/GMController.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System.Threading;
+using UnityEngine.SceneManagement;
 
 public class GMController : MonoBehaviour
 {
+    private const float PLAYTIME = 120.0f;
+    [SerializeField] GameObject timerText = null;
+    [SerializeField] GameObject scoreText = null;
+    [SerializeField] GameObject itemText = null;
+    private float timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = PLAYTIME;
     }
 
     // Update is called once per frame
     void Update()
     {
+        playTimer();
 
+    }
+    private void playTimer()
+    {
+        timer -= Time.deltaTime;
+        timerText.GetComponent<TextMeshProUGUI>().text = "Time:" + timer.ToString("F2");
+        if(timer < 0.0f)
+        {
+            SceneManager.LoadScene("EndScene");
+        }
     }
 }
